8dcc8f55945482f3fd58f17b5975a386ffbd2c21
hdouhua <hdouhua@gmail.com>
2020-03-15 17:26:05 +0800

    [add]: leetcode problem - 20

---
+++ src/app/leetcode/l20/Solution.java
@@ -0,0 +1,39 @@
+package app.leetcode;
+
+import java.util.Stack;
+
+/**
+ * ValidParentheses
+ * or more, please refer to https://leetcode.com/problems/valid-parentheses/
+ */
+class Solution {
+    public boolean isValid(String s){
+        Stack<Character> stack = new Stack<>();
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if(c == '{' || c == '[' || c == '(') {
+                stack.push(c);
+            } else if (c == '}' || c == ']' || c == ')') {
+                if(stack.isEmpty()){
+                    return false;
+                }
+                char top = stack.pop();
+                if (c == '}' && top != '{'){
+                    return false;
+                }
+                if (c == ']' && top != '['){
+                    return false;
+                }
+                if (c == ')' && top != '('){
+                    return false;
+                }
+            }
+        }
+        return stack.isEmpty();
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new Solution().isValid("[]{}]"));
+        System.out.println(new Solution().isValid("[]{}[()]"));
+    }
+}
