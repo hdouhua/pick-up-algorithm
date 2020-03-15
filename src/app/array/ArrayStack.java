0d96b5a3ec2a0463397c347756ae07774865e176
hdouhua <hdouhua@gmail.com>
2020-03-15 17:17:00 +0800

    [add]: stack based on array

---
+++ src/app/array/ArrayStack.java
@@ -0,0 +1,62 @@
+package app.array;
+
+/**
+ * Stack
+ */
+public class ArrayStack<E> implements Stack<E> {
+
+    Array<E> array;
+
+    public ArrayStack() {
+        array = new Array<>();
+    }
+
+    public ArrayStack(int capacity) {
+        array = new Array<>(capacity);
+    }
+    
+    @Override
+    public int getSize() {
+        return array.getSize();
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return array.isEmpty();
+    }
+
+    public int getCapacity(){
+        return array.getCapacity();
+    }
+
+    @Override
+    public void push(E e) {
+        array.addLast(e);
+    }
+
+    @Override
+    public E pop() {
+        return array.removeLast();
+    }
+
+    @Override
+    public E peek() {
+        return array.getLast();
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("Stack: ");
+        sb.append("[");
+        for (int i = 0; i < array.getSize(); i++) {
+            if(i != 0) {
+                sb.append(",");
+            }
+            sb.append(array.get(i));
+        }
+        sb.append("] top");
+        return sb.toString();
+    }
+
+}
